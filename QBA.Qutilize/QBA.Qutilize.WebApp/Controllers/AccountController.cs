6f36130ad2de44237dcbb7d72313411c9f800ff6
alokekdatta <alokekdatta@gmail.com>
2019-02-15 17:53:53 +0530

    WebApp
    WebApp

---
+++ QBA.Qutilize/QBA.Qutilize.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using QBA.Qutilize.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QBA.Qutilize.WebApp.DAL;
+using System.Data;
+using QBA.Qutilize.WebApp.Helper;
+
+namespace QBA.Qutilize.WebApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        
+    }
+}
---
+++ QBA.Qutilize/QBA.Qutilize.WebApp/Models/AccountViewModels.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using QBA.Qutilize.WebApp.DAL;
+
+namespace QBA.Qutilize.WebApp.Models
+{
+    public class AccountViewModels
+    {
+    }
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "User ID")]
+        public string UserID { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        //[Display(Name = "Remember me?")]
+        //public bool RememberMe { get; set; }
+
+        public DataTable VerifyLogin(string userName, string password)
+        {
+            DataTable dt = null;
+            try
+            {
+                dt = DataAccess.VerifyLogin(userName, password);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return dt;
+        }
+        public DataSet GetDashBoardData(int userID, DateTime startDate, DateTime endDate)
+        {
+            DataSet ds = null;
+            try
+            {
+                ds = DataAccess.GetDashBoardData(userID,  startDate,  endDate);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return ds;
+        }
+    }
+    
+}
