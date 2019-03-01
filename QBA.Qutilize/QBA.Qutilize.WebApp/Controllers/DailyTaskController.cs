07e6ad021ee3cb68f7fff72887802018065ed123
rahul-qba <rahul.qba@outlook.com>
2019-03-01 20:25:40 +0530

    added daily task controller and view page

---
+++ QBA.Qutilize/QBA.Qutilize.WebApp/Controllers/DailyTaskController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QBA.Qutilize.WebApp.Controllers
+{
+    public class DailyTaskController : Controller
+    {
+        // GET: DailyTask
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ QBA.Qutilize/QBA.Qutilize.WebApp/Models/DailyTaskModel.cs
@@ -0,0 +1,32 @@
+﻿using QBA.Qutilize.WebApp.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QBA.Qutilize.WebApp.Models
+{
+    public class DailyTaskModel
+    {
+        public int DailyTaskId { get; set; }
+        public int UserID { get; set; }
+        public int ProjectID { get; set; }
+        public string ProjectName { get; set; }
+        public DateTime TaskDate { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string TaskName { get; set; }
+        public string Discription { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string EditedBy { get; set; }
+        public DateTime? EditedDate { get; set; }
+        public bool IsActive { get; set; }
+        public bool ISErr { get; set; }
+        public string ErrString { get; set; }
+
+        #region Global Variable Decleartion::
+        SqlHelper objSQLHelper = new SqlHelper();
+        #endregion
+    }
+}
