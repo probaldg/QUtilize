8c6057155808b15a6e941327af0ea203c11cbeb8
alokekdatta <alokekdatta@gmail.com>
2019-03-07 20:20:00 +0530

    update

--- QBA.Qutilize/QBA.Qutilize.WebApp/Controllers/HomeController.cs
+++ QBA.Qutilize/QBA.Qutilize.WebApp/Controllers/HomeController.cs
@@ -150,26 +150,43 @@ namespace QBA.Qutilize.WebApp.Controllers
             StringBuilder sbOut = new StringBuilder();
             try
             {
-                DayOfWeek day = DateTime.Now.DayOfWeek;
-                int days = day - DayOfWeek.Monday;
-                DateTime startdate = DateTime.Now.AddDays(-days);
-                DateTime endDate = startdate.AddDays(6);
+                DateTime startdate = DateTime.Now;
+                DateTime endDate = DateTime.Now;
+                if (Session["DateRange"] == null)
+                {
+                    DayOfWeek day = DateTime.Now.DayOfWeek;
+                    int days = day - DayOfWeek.Monday;
+                    startdate = DateTime.Now.AddDays(-days);
+                    endDate = startdate.AddDays(6);
+                    Session.Add("DateRange", startdate + "|" + endDate);
+                }
+                else
+                {
+                    string[] arrdate = Session["DateRange"].ToString().Split('|');
+                    startdate =Convert.ToDateTime(arrdate[0]);
+                    endDate = Convert.ToDateTime(arrdate[1]);
+                }
                 LoginViewModel lvm = new LoginViewModel();
                 DataSet ds = lvm.GetDashBoardData(Convert.ToInt32(Session["sessUser"]), startdate, endDate);
                 if (ds != null && ds.Tables.Count > 0 && ds.Tables[0] != null && ds.Tables[0].Rows.Count > 0)
                 {
                     Session.Add("DashBoardDetail", ds);
                 }
-                sbOut.Append("<table id='tblSearch'>");
-                sbOut.Append("<tr>");
-                sbOut.Append("<td><label>Date range:</label></td>");
-                sbOut.Append("<td><label>Start Date:</label></td>");
-                sbOut.Append("<td><input type='text' class='form-control input-sm datepicker hasDatepicker' id='txtStartDate' value='" + startdate.ToShortDateString() + "'  /></td>");
-                sbOut.Append("<td><label>End Date:</label></td>");
-                sbOut.Append("<td><input type='text' class='form-control input-sm datepicker hasDatepicker' id='txtEndDate' value='" + endDate.ToShortDateString() + "' /></td>");
-                sbOut.Append("<td><input type='submit' id='btnSearch' value='Show' name='btnSearch' class='btn btn-default' onclick='RefreshData();' /></td>");
-                sbOut.Append("</tr>");
-                sbOut.Append("</table>");
+                sbOut.Append("<div class='form-group col-md-12'>");
+                sbOut.Append("<label class='control-label col-md-2'>Start Date: </label>");
+                sbOut.Append("<div class='col-md-2'>");
+                sbOut.Append("<input type='text' class='form-control' id='txtStartDate' value='" + startdate.ToShortDateString() + "'  />");
+                sbOut.Append("</div>");
+                sbOut.Append("<label class='control-label col-md-2'>End Date: </label>");
+                sbOut.Append("<div class='col-md-2'>");
+                sbOut.Append("<input type='text' class='form-control' id='txtEndDate' value='" + endDate.ToShortDateString() + "' />");
+                sbOut.Append("</div>");
+                sbOut.Append("<div class='col-md-2'>");
+                sbOut.Append("<input type='submit' id='btnSearch' value='Show' name='btnSearch' class='btn btn-primary' onclick='RefreshData();' />");
+                sbOut.Append("</div>");
+                sbOut.Append("</div>");
+                sbOut.Append("<hr>");
+
             }
             catch (Exception exx)
             { }
@@ -191,7 +208,7 @@ namespace QBA.Qutilize.WebApp.Controllers
                     _chart.labels = arrrayDate;
                     _chart.datasets = new List<Datasets>();
                     List<Datasets> _dataSet = new List<Datasets>();
-                    string[] arrbg = new string[] { "#f39c12", "#00c0ef", "#0073b7", "#3c8dbc", "#00a65a", "#001f3f", "#39cccc", "#3d9970", "#01ff70", "#ff851b", "#f012be", "#605ca8", "#d81b60", "#020219", "#07074c", "#0f0f99", "#1616e5", "#4646ff", "#8c8cff", "#d1d1ff", "#a3a3ff", "#babaff", "#d1d1ff", "#e8e8ff", "#E6FCDD", "#EFF7B5", "#EFB5F7" };
+                    string[] arrbg = new string[] {  "#f39c12", "#00c0ef", "#0073b7", "#3c8dbc", "#00a65a", "#001f3f", "#39cccc", "#3d9970", "#01ff70", "#ff851b", "#f012be", "#605ca8", "#d81b60", "#020219", "#07074c", "#0f0f99", "#1616e5", "#4646ff", "#8c8cff", "#d1d1ff", "#a3a3ff", "#babaff", "#d1d1ff", "#e8e8ff", "#E6FCDD", "#EFF7B5", "#EFB5F7",    "#194C66", "#1F5D7C", "#246E93", "#2A7FAA", "#3090C0", "#3E9ECE", "#55AAD4", "#6BB5DA", "#82C0DF"};
                     int intColor = 0;
                     foreach (string stproj in arrrayProj)
                     {
@@ -324,7 +341,7 @@ namespace QBA.Qutilize.WebApp.Controllers
                     {
                         label = "",
                         data = arrData,// new string[] { "28", "48", "40", "19", " 86", "27", "90" },
-                        backgroundColor = new string[] { "#f39c12", "#00c0ef", "#0073b7", "#3c8dbc", "#00a65a", "#001f3f", "#39cccc", "#3d9970", "#01ff70", "#ff851b", "#f012be", "#605ca8", "#d81b60",  "#020219", "#07074c", "#0f0f99", "#1616e5", "#4646ff", "#8c8cff", "#d1d1ff", "#a3a3ff", "#babaff", "#d1d1ff", "#e8e8ff", "#1A5276", "#27AE60" },
+                        backgroundColor = new string[] { "#f39c12", "#00c0ef", "#0073b7", "#3c8dbc", "#00a65a", "#001f3f", "#39cccc", "#3d9970", "#01ff70", "#ff851b", "#f012be", "#605ca8", "#d81b60",  "#020219", "#07074c", "#0f0f99", "#1616e5", "#4646ff", "#8c8cff", "#d1d1ff", "#a3a3ff", "#babaff", "#d1d1ff", "#e8e8ff", "#1A5276", "#27AE60", "#65184B", "#7C1E5C", "#93246D", "#A9297E", "#C02F8F", "#CF3C9D", "#D453A9", "#DA6AB4", "#E081C0"},
                         borderColor = new string[] { "#020219", "#800000", "#808000", "#008080", "#800080", "#0000FF", "#000080", "#999999", "#E9967A", "#CD5C5C", "#1A5276", "#27AE60" },
                         borderWidth = "1"
                     });
@@ -355,51 +372,54 @@ namespace QBA.Qutilize.WebApp.Controllers
             try
             {
                 Session.Remove("DashBoardDetail");
-                LoginViewModel lvm = new LoginViewModel();
-                DataSet dsNew = lvm.GetDashBoardData(Convert.ToInt32(Session["sessUser"]),Convert.ToDateTime(startdate),Convert.ToDateTime(endDate));
-                Session.Add("DashBoardDetail", dsNew);
+                if (Session["DateRange"] == null) Session.Remove("DateRange");
+                Session.Add("DateRange", startdate + "|" + endDate);
+                sbContent.Append("Reload");
+                //LoginViewModel lvm = new LoginViewModel();
+                //DataSet dsNew = lvm.GetDashBoardData(Convert.ToInt32(Session["sessUser"]),Convert.ToDateTime(startdate),Convert.ToDateTime(endDate));
+                //Session.Add("DashBoardDetail", dsNew);
 
-                DataSet ds = (DataSet)Session["DashBoardDetail"];
-                if (ds != null && ds.Tables.Count > 0 && ds.Tables[0] != null && ds.Tables[0].Rows.Count > 0)
-                {
-                    DataTable uniqueColsProj = ds.Tables[0].DefaultView.ToTable(true, "projectName");
-                    string[] arrrayProj = uniqueColsProj.Rows.OfType<DataRow>().Select(k => k[0].ToString()).ToArray();
-                    DataTable uniqueColsDate = ds.Tables[0].DefaultView.ToTable(true, "Date");
-                    string[] arrrayDate = uniqueColsDate.Rows.OfType<DataRow>().Select(k => k[0].ToString()).ToArray();
-                    //sbContent.Append("<div class='panel-body dvBorder form-group'>");
-                    sbContent.Append("<div class='table-responsive'>");
-                    sbContent.Append("<table class='table table-bordered' id='tblAppraisalRating'  width='100%'>");
-                    sbContent.Append("<thead>");
-                    sbContent.Append("<tr>");
-                    sbContent.Append("<th class='text-center tblHeaderColor'>Date</th>");
-                    foreach (string strProjName in arrrayProj)
-                    {
-                        sbContent.Append("<th class='text-center tblHeaderColor'>" + strProjName + "</th>");
-                    }
-                    sbContent.Append("</tr>");
-                    sbContent.Append("</thead>");
-                    sbContent.Append("<tbody id='tbodyDateWiseData'>");
-                    foreach (string stDate in arrrayDate)
-                    {
-                        sbContent.Append("<tr>");
-                        sbContent.Append("<td><span class='control-text'>" + stDate + "</span></td>");
-                        foreach (string stproj in arrrayProj)
-                        {
-                            DataRow[] result = ds.Tables[0].Select("projectName = '" + stproj + "' AND Date = '" + stDate + "'");
-                            if (result.Length > 0)
-                            {
-                                sbContent.Append("<td><span class='control-text'>" + result[0]["hms"] + "</span></td>");
-                            }
-                            else
-                            { sbContent.Append("<td><span class='control-text'> - </span></td>"); }
-                        }
-                        sbContent.Append("</tr>");
-                    }
-                    sbContent.Append("</tbody>");
-                    sbContent.Append("</table>");
-                    sbContent.Append("</div>");
-                    //sbContent.Append("</div>");
-                }
+                //DataSet ds = (DataSet)Session["DashBoardDetail"];
+                //if (ds != null && ds.Tables.Count > 0 && ds.Tables[0] != null && ds.Tables[0].Rows.Count > 0)
+                //{
+                //    DataTable uniqueColsProj = ds.Tables[0].DefaultView.ToTable(true, "projectName");
+                //    string[] arrrayProj = uniqueColsProj.Rows.OfType<DataRow>().Select(k => k[0].ToString()).ToArray();
+                //    DataTable uniqueColsDate = ds.Tables[0].DefaultView.ToTable(true, "Date");
+                //    string[] arrrayDate = uniqueColsDate.Rows.OfType<DataRow>().Select(k => k[0].ToString()).ToArray();
+                //    //sbContent.Append("<div class='panel-body dvBorder form-group'>");
+                //    sbContent.Append("<div class='table-responsive'>");
+                //    sbContent.Append("<table class='table table-bordered' id='tblAppraisalRating'  width='100%'>");
+                //    sbContent.Append("<thead>");
+                //    sbContent.Append("<tr>");
+                //    sbContent.Append("<th class='text-center tblHeaderColor'>Date</th>");
+                //    foreach (string strProjName in arrrayProj)
+                //    {
+                //        sbContent.Append("<th class='text-center tblHeaderColor'>" + strProjName + "</th>");
+                //    }
+                //    sbContent.Append("</tr>");
+                //    sbContent.Append("</thead>");
+                //    sbContent.Append("<tbody id='tbodyDateWiseData'>");
+                //    foreach (string stDate in arrrayDate)
+                //    {
+                //        sbContent.Append("<tr>");
+                //        sbContent.Append("<td><span class='control-text'>" + stDate + "</span></td>");
+                //        foreach (string stproj in arrrayProj)
+                //        {
+                //            DataRow[] result = ds.Tables[0].Select("projectName = '" + stproj + "' AND Date = '" + stDate + "'");
+                //            if (result.Length > 0)
+                //            {
+                //                sbContent.Append("<td><span class='control-text'>" + result[0]["hms"] + "</span></td>");
+                //            }
+                //            else
+                //            { sbContent.Append("<td><span class='control-text'> - </span></td>"); }
+                //        }
+                //        sbContent.Append("</tr>");
+                //    }
+                //    sbContent.Append("</tbody>");
+                //    sbContent.Append("</table>");
+                //    sbContent.Append("</div>");
+                //    //sbContent.Append("</div>");
+                //}
             }
             catch (Exception exx) { }
             return Json(sbContent.ToString());
